006cf5ed582b483c5d3fa5de373870f0873fae47
yus-ko <yus-ko>
2023-12-07 11:08:00 +0900

    robot_localization

--- potbot_localization/src/twist_to_odometry.cpp
+++ potbot_localization/src/twist_to_odometry.cpp
@@ -1,81 +1,119 @@
 // #include<potbot_localization/Localization.h>
-// #include <potbot_lib/Utility.h>
+#include <potbot_lib/Utility.h>
 #include <ros/ros.h>
 // #include <ros/package.h>
-#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_broadcaster.h>
 // #include <tf2_ros/static_transform_broadcaster.h>
 // #include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/Imu.h>
 
 std::string G_FRAME_ID_MAP			= "map",
 			G_FRAME_ID_ODOM			= "odom",
-			G_FRAME_ID_ROBOT_BASE	= "base_footprint",
-			G_TOPIC_TWIST			= "cmd_vel",
-			G_TOPIC_ODOM			= "odom";
+			G_FRAME_ID_ROBOT_BASE	= "base_link",
+			G_TOPIC_SUB_TWIST		= "cmd_vel",
+			G_TOPIC_PUB_ODOM		= "odom";
 
-ros::Publisher g_pub_odom;
-nav_msgs::Odometry g_odom;
+ros::Publisher g_pub_odom, g_pub_odom_rectangle, g_pub_odom_trapezoid, g_pub_imu;
+nav_msgs::Odometry g_odom, g_odom_rectangle, g_odom_trapezoid;
 
-void get_RPY(geometry_msgs::Quaternion orientation, double &roll, double &pitch, double &yaw)
+void dead_reckoning_rectangle(geometry_msgs::TwistStamped vel_msg)
 {
-	tf2::Quaternion quat;
-	tf2::convert(orientation, quat);
-	tf2::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+	g_odom_rectangle.header = vel_msg.header;
+	g_odom_rectangle.twist.twist = vel_msg.twist;
+
+	static double t_pre = g_odom_rectangle.header.stamp.toSec();
+	double dt = g_odom_rectangle.header.stamp.toSec() - t_pre;
+
+	double v = g_odom_rectangle.twist.twist.linear.x;
+	double omega = g_odom_rectangle.twist.twist.angular.z;
+	double theta = potbot_lib::utility::get_Yaw(g_odom_rectangle.pose.pose.orientation) + omega*dt;
+	double x = g_odom_rectangle.pose.pose.position.x + v*cos(theta)*dt;
+	double y = g_odom_rectangle.pose.pose.position.y + v*sin(theta)*dt;
+
+	g_odom_rectangle.pose.pose.position.x = x;
+	g_odom_rectangle.pose.pose.position.y = y;
+	g_odom_rectangle.pose.pose.orientation = potbot_lib::utility::get_Quat(0,0,theta);
+
+	g_pub_odom_rectangle.publish(g_odom_rectangle);
+
+	t_pre = g_odom_rectangle.header.stamp.toSec();
+
+	geometry_msgs::TransformStamped tf_base_footprint;
+
+    tf_base_footprint.header = g_odom_rectangle.header;
+    tf_base_footprint.child_frame_id = G_FRAME_ID_ROBOT_BASE;
+    tf_base_footprint.transform.translation.x = g_odom_rectangle.pose.pose.position.x;
+    tf_base_footprint.transform.translation.y = g_odom_rectangle.pose.pose.position.y;
+    tf_base_footprint.transform.translation.z = g_odom_rectangle.pose.pose.position.z;
+    tf_base_footprint.transform.rotation = g_odom_rectangle.pose.pose.orientation;
+	static tf2_ros::TransformBroadcaster g_broadcaster;
+    // g_broadcaster.sendTransform(tf_base_footprint);
 }
 
-geometry_msgs::Quaternion get_Quat(double roll, double pitch, double yaw)
+void dead_reckoning_trapezoid(geometry_msgs::TwistStamped vel_msg)
 {
-	tf2::Quaternion quat;
-	quat.setRPY(roll, pitch, yaw);
-	geometry_msgs::Quaternion orientation;
-	tf2::convert(quat, orientation);
-	return orientation;
-}
+	g_odom_trapezoid.header = vel_msg.header;
+	g_odom_trapezoid.twist.twist = vel_msg.twist;
 
-double get_Yaw(geometry_msgs::Quaternion orientation)
-{
-	double roll, pitch, yaw;
-	get_RPY(orientation, roll, pitch, yaw);
-	return yaw;
+	static double t_pre = g_odom_trapezoid.header.stamp.toSec();
+	double dt = g_odom_trapezoid.header.stamp.toSec() - t_pre;
+
+	static double v_pre = g_odom_trapezoid.twist.twist.linear.x;
+	double v_now = g_odom_trapezoid.twist.twist.linear.x;
+
+	static double omega_pre = g_odom_trapezoid.twist.twist.angular.z;
+	double omega_now = g_odom_trapezoid.twist.twist.angular.z;
+
+	double theta = potbot_lib::utility::get_Yaw(g_odom_trapezoid.pose.pose.orientation) + ((omega_now + omega_pre)*dt/2);
+	double x = g_odom_trapezoid.pose.pose.position.x + ((v_now + v_pre)*dt/2)*cos(theta);
+	double y = g_odom_trapezoid.pose.pose.position.y + ((v_now + v_pre)*dt/2)*sin(theta);
+
+	g_odom_trapezoid.pose.pose.position.x = x;
+	g_odom_trapezoid.pose.pose.position.y = y;
+	g_odom_trapezoid.pose.pose.orientation = potbot_lib::utility::get_Quat(0,0,theta);
+
+	g_pub_odom_trapezoid.publish(g_odom_trapezoid);
+	
+	t_pre = g_odom_trapezoid.header.stamp.toSec();
+	v_pre = v_now;
+	omega_pre = omega_now;
 }
 
 void twist_callback(const geometry_msgs::Twist& msg)
 {
 	// ROS_INFO("twist callback");
-	g_odom.header.frame_id = G_FRAME_ID_ODOM;
-	g_odom.header.stamp = ros::Time::now();
+	geometry_msgs::TwistStamped tm;
+	tm.header.frame_id = G_FRAME_ID_ODOM;
+	tm.header.stamp = ros::Time::now();
+	tm.twist = msg;
 
-	static double t_pre = g_odom.header.stamp.toSec();
-	double dt = g_odom.header.stamp.toSec() - t_pre;
+	dead_reckoning_rectangle(tm);
+	dead_reckoning_trapezoid(tm);
+}
 
-	g_odom.twist.twist = msg;
+void odom_twist_callback(const nav_msgs::Odometry& msg)
+{
+	// ROS_INFO("odom twist callback");
+	geometry_msgs::TwistStamped tm;
+	tm.header = msg.header;
+	tm.twist = msg.twist.twist;
+	// tm.twist.linear.x *= 1.1;
+	// tm.twist.angular.z *= 1.1;
 
-	double v = g_odom.twist.twist.linear.x;
-	double omega = g_odom.twist.twist.angular.z;
-	double theta = get_Yaw(g_odom.pose.pose.orientation) + omega*dt;
-	double x = g_odom.pose.pose.position.x + v*cos(theta)*dt;
-	double y = g_odom.pose.pose.position.y + v*sin(theta)*dt;
+	dead_reckoning_rectangle(tm);
+	dead_reckoning_trapezoid(tm);
+}
 
-	g_odom.pose.pose.position.x = x;
-	g_odom.pose.pose.position.y = y;
-	g_odom.pose.pose.orientation = get_Quat(0,0,theta);
+void imu_callback(const sensor_msgs::Imu& msg)
+{
+	// ROS_INFO("imu callback");
+	sensor_msgs::Imu imu = msg;
+	imu.header.frame_id = "robot_5/imu_link";
+	g_pub_imu.publish(imu);
 
-	g_pub_odom.publish(g_odom);
-
-	t_pre = g_odom.header.stamp.toSec();
-
-	geometry_msgs::TransformStamped tf_base_footprint;
-
-    tf_base_footprint.header = g_odom.header;
-    tf_base_footprint.child_frame_id = G_FRAME_ID_ROBOT_BASE;
-    tf_base_footprint.transform.translation.x = g_odom.pose.pose.position.x;
-    tf_base_footprint.transform.translation.y = g_odom.pose.pose.position.y;
-    tf_base_footprint.transform.translation.z = g_odom.pose.pose.position.z;
-    tf_base_footprint.transform.rotation = g_odom.pose.pose.orientation;
-	static tf2_ros::TransformBroadcaster g_broadcaster;
-    g_broadcaster.sendTransform(tf_base_footprint);
 }
 
 int main(int argc,char **argv){
@@ -89,8 +127,8 @@ int main(int argc,char **argv){
 	n.getParam("FRAME_ID/MAP",			G_FRAME_ID_MAP);
 	n.getParam("FRAME_ID/ODOM",			G_FRAME_ID_ODOM);
 	n.getParam("FRAME_ID/ROBOT_BASE",	G_FRAME_ID_ROBOT_BASE);
-	n.getParam("TOPIC/TWIST",			G_TOPIC_TWIST);
-	n.getParam("TOPIC/ODOM",			G_TOPIC_ODOM);
+	n.getParam("TOPIC/SUB/TWIST",		G_TOPIC_SUB_TWIST);
+	n.getParam("TOPIC/PUB/ODOM",		G_TOPIC_PUB_ODOM);
 	n.getParam("INITIAL_POSE/X",		x);
 	n.getParam("INITIAL_POSE/Y",		y);
 	n.getParam("INITIAL_POSE/Z",		z);
@@ -99,15 +137,20 @@ int main(int argc,char **argv){
 	n.getParam("INITIAL_POSE/YAW",		yaw);
 
 	ros::NodeHandle nh;
-	ros::Subscriber sub_twist = nh.subscribe(G_TOPIC_TWIST,1,&twist_callback);
-	g_pub_odom	 = nh.advertise<nav_msgs::Odometry>(G_TOPIC_ODOM, 1);
+	ros::Subscriber sub_twist = nh.subscribe(G_TOPIC_SUB_TWIST,1,&twist_callback);
+	ros::Subscriber sub_odom_twist = nh.subscribe("/robot_5/odom",1,&odom_twist_callback);
+	ros::Subscriber sub_imu = nh.subscribe("/robot_5/mynteye/imu/data_raw",1,&imu_callback);
+	g_pub_odom	 = nh.advertise<nav_msgs::Odometry>(G_TOPIC_PUB_ODOM, 1);
+	g_pub_odom_rectangle	 = nh.advertise<nav_msgs::Odometry>("/gazebo_dead_reckoning/rectangle", 1);
+	g_pub_odom_trapezoid	 = nh.advertise<nav_msgs::Odometry>("/gazebo_dead_reckoning/trapezoid", 1);
+	g_pub_imu	 = nh.advertise<sensor_msgs::Imu>("/robot_5/imu", 1);
 
 	nav_msgs::Odometry odom_init;
 	odom_init.pose.pose.position.x = 0;
 	odom_init.pose.pose.position.y = 0;
 	odom_init.pose.pose.position.z = 0;
-	odom_init.pose.pose.orientation = get_Quat(0,0,0);
-	g_odom = odom_init;
+	odom_init.pose.pose.orientation = potbot_lib::utility::get_Quat(0,0,0);
+	g_odom = g_odom_rectangle = g_odom_trapezoid = odom_init;
 
 	ros::spin();
 	
